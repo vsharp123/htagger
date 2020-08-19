8349b7caa628df496923e07923bd71a915eb4da7
Vignesh <vikee.email@gmail.com>
2020-08-19 12:50:50 +0530

    Improved while loop and formatting.
    Special thanks to @Snaacky for helping.

--- nhscript.py
+++ nhscript.py
@@ -1,33 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
-    
+
+
 def print_info(soup, url, site_name):
-    try:    
+    try:
         print(site_name)
-        print("Your url:"+url)
+        print("Your url:" + url)
         print(soup.find(id='tags').text)
     except AttributeError:
-        print("Not found on "+site_name)
+        print("Not found on " + site_name)
 
-x=False
-while x is False: 
+
+while True:
     print("Enter sacred 6 digit moolah:")
-    moolah=input()
-    x = moolah.isnumeric()
-#moolah=str(random.randint(0,999999))
-#setting various urls
-urlnh="https://nhentai.net/g/"+moolah
-url9h="https://9hentai.com/g/"+moolah
-urlnyh="https://nyahentai.com/g/"+moolah
-#fetching stuff
-matternh=requests.get(urlnh).text
-matter9h=requests.get(url9h).text
-matternyh=requests.get(urlnyh).text
-#brewing some soup
-soupnh = BeautifulSoup(matternh, 'html.parser')
-soup9h = BeautifulSoup(matter9h, 'html.parser')
-soupnyh = BeautifulSoup(matternyh, 'html.parser')
-#printing...wish me luck
-print_info(soupnh, urlnh, "nhentai")
-print_info(soup9h, url9h, "9hentai")
-print_info(soupnyh, urlnyh, "nyahentai")
+    moolah = input()
+    if moolah.isnumeric():
+        urlnh = "https://nhentai.net/g/" + moolah
+        url9h = "https://9hentai.com/g/" + moolah
+        urlnyh = "https://nyahentai.com/g/" + moolah
+
+        # fetching stuff
+        matternh = requests.get(urlnh).text
+        matter9h = requests.get(url9h).text
+        matternyh = requests.get(urlnyh).text
+
+        # brewing some soup
+        soupnh = BeautifulSoup(matternh, 'html.parser')
+        soup9h = BeautifulSoup(matter9h, 'html.parser')
+        soupnyh = BeautifulSoup(matternyh, 'html.parser')
+
+        # printing...wish me luck
+        print_info(soupnh, urlnh, "nhentai")
+        print_info(soup9h, url9h, "9hentai")
+        print_info(soupnyh, urlnyh, "nyahentai")
+        break
