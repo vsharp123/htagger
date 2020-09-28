060d33a0336c5df509878740e8903b3df57fefba
[REDACTED] <vikee.email@gmail.com>
2020-09-28 16:55:07 +0530

    Fixed the descriptors when a manga wasn't found.
    Now, the URL won't print if the manga doesn't exist.

--- nhscript.py
+++ nhscript.py
@@ -1,39 +1,47 @@
 from bs4 import BeautifulSoup
 import requests
 import argparse
+import random
 
 def print_info(soup, url, site_name):
     try:
-        print(site_name)
-        print("Your url:" + url)
-        print(soup.find(id='tags').text)
+        data=soup.find(id='tags').text
     except AttributeError:
-        print("Not found on " + site_name)
+        print("Requested manga not found on "+site_name+". \n")
+        return 0
+    
+    print(site_name)
+    print("Your url:" + url)
+    print(data+"\n")
 
 
-while True:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("manga_id", help="ID of the manga.", type=int)
-    data=parser.parse_args()    
-    moolah = str(data.manga_id)
-        
-    if moolah.isnumeric():
-        urlnh = "https://nhentai.net/g/" + moolah
-        url9h = "https://9hentai.com/g/" + moolah
-        urlnyh = "https://nyahentai.com/g/" + moolah
+def main():
+    while True:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("manga_id", help="ID of the manga.", type=int)
+        data=parser.parse_args()    
+        moolah = str(data.manga_id)
+            
+        if moolah.isnumeric():
+            urlnh = "https://nhentai.net/g/" + moolah
+            url9h = "https://9hentai.com/g/" + moolah
+            urlnyh = "https://nyahentai.com/g/" + moolah
 
-        # fetching stuff
-        matternh = requests.get(urlnh).text
-        matter9h = requests.get(url9h).text
-        matternyh = requests.get(urlnyh).text
+            # fetching stuff
+            matternh = requests.get(urlnh).text
+            matter9h = requests.get(url9h).text
+            matternyh = requests.get(urlnyh).text
 
-        # brewing some soup
-        soupnh = BeautifulSoup(matternh, 'html.parser')
-        soup9h = BeautifulSoup(matter9h, 'html.parser')
-        soupnyh = BeautifulSoup(matternyh, 'html.parser')
+            # brewing some soup
+            soupnh = BeautifulSoup(matternh, 'html.parser')
+            soup9h = BeautifulSoup(matter9h, 'html.parser')
+            soupnyh = BeautifulSoup(matternyh, 'html.parser')
 
-        # printing...wish me luck
-        print_info(soupnh, urlnh, "nhentai")
-        print_info(soup9h, url9h, "9hentai")
-        print_info(soupnyh, urlnyh, "nyahentai")
-        break
+            # printing...wish me luck
+            print_info(soupnh, urlnh, "nhentai")
+            print_info(soup9h, url9h, "9hentai")
+            print_info(soupnyh, urlnyh, "nyahentai")
+            break
+
+
+main()
