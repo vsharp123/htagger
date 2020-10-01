ec56a1733b7c8cc737eed6140e8fd146e7d96846
JesseyWhite <49629699+JesseyWhite@users.noreply.github.com>
2020-10-01 00:29:25 -0500

    Update nhscript.py
    rewrite of the function

--- nhscript.py
+++ nhscript.py
@@ -1,48 +1,45 @@
 from bs4 import BeautifulSoup
 import requests
 import argparse
-import random
 
 
-def print_info(soup, url, site_name):
+def print_info(page_id: int, site: str):
     try:
-        data = soup.find(id='tags').text
+        data = soupify(fetch_website(manga(page_id, site)))
     except AttributeError:
-        print(f"Requested manga not found on {site_name}. \n")
-        return 0
+        print(f"Requested manga not found on {site}. \n")
+        return
+    
+    print(f'{site}\nYour url: {manga(page_id, site)}\n{data}\n')
 
-    print(site_name)
-    print(f"Your url: {url}")
-    print(f"{data}\n")
+def manga(page: int, source: str):
+    """Returns the correct URL for the given source"""
+    if source == 'nhentai':
+        return f"https://nhentai.net/g/{page}"
+    elif source == '9hentai':
+        return f"https://9hentai.com/g/{page}"
+    elif source == 'nyahentai':
+        return f"https://nyahentai.com/g/{page}"
+    
+        
+def fetch_website(url: str):
+    """Loading website and returning the HTML"""
+    return requests.get(url).text
+
+
+def soupify(soup):
+    """Parses HTML into readable format via BeautifulSoup"""
+    return BeautifulSoup(soup, 'html.parser')
 
 
 if __name__ == '__main__':
-    while True:
-        parser = argparse.ArgumentParser()
-        parser.add_argument("manga_id", help="ID of the manga.", type=int)
-        data = parser.parse_args()
-        moolah = str(data.manga_id)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("manga_id", help="ID of the manga.", type=int)
+    data = parser.parse_args()
+    manga_id = str(data.manga_id)
 
-        if moolah.isnumeric():
-            urlnh = f"https://nhentai.net/g/{moolah}"
-            url9h = f"https://9hentai.com/g/{moolah}"
-            urlnyh = f"https://nyahentai.com/g/{moolah}"
-
-            # fetching stuff
-            matternh = requests.get(urlnh).text
-            matter9h = requests.get(url9h).text
-            matternyh = requests.get(urlnyh).text
-
-            # brewing some soup
-            soupnh = BeautifulSoup(matternh, 'html.parser')
-            soup9h = BeautifulSoup(matter9h, 'html.parser')
-            soupnyh = BeautifulSoup(matternyh, 'html.parser')
-
-            # printing...wish me luck
-            print_info(soupnh, urlnh, "nhentai")
-            print_info(soup9h, url9h, "9hentai")
-            print_info(soupnyh, urlnyh, "nyahentai")
-            break
-        else:
-            print(f'moolah ({moolah}) is not numeric')
-            break
+    if not manga_id.isnumeric():
+        print(f'moolah ({manga_id}) is not numeric')
+        exit()
+    for x in {'nyahentai', '9hentai', 'nhentai'}:
+        print_info(manga_id, x)
